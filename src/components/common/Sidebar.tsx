ac8094f278703897cda067a7dd4e5496e5b08ca7
jngmnj <exorbitance@naver.com>
2024-09-02 23:57:25 +0900

    feat: sidebar 작성중

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,10 +1,27 @@
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div>
-
+    <div className="absolute left-0 top-0 z-10 h-full w-full border border-blue-600 bg-gray-100 md:block">
+      <Link href="/">
+        <h1 className="text-2xl font-bold">jngmnj</h1>
+      </Link>
+      <div className="hidden items-center justify-center lg:flex">
+        <Link href="/about">
+          <div className="p-4">About</div>
+        </Link>
+        <Link href="/projects">
+          <div className="p-4">Projects</div>
+        </Link>
+        <Link href="/blog">
+          <div className="p-4">Blog</div>
+        </Link>
+        <Link href="/contact">
+          <div className="p-4">Contact</div>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
