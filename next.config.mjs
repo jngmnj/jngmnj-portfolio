73fd62bbf20d98838ba32fc0d474342e94bf893d
jngmnj <exorbitance@naver.com>
2025-09-23 16:27:23 +0900

    chore: Next.js 15.5.3 및 React 19로 업그레이드(#1)
    - `npx @next/codemod@canary upgrade latest` 명령어로 Next.js 15로 코드베이스 업그레이드
    - `next`, `eslint-config-next`, `react`, `react-dom`, 관련 `@types/*` 패키지 버전 업데이트
    - `dev` 스크립트에 `--turbopack` 옵션 추가
    - `tailwind`, `typescript`, 기타 의존성 최신 버전으로 유지
    - `overrides` 필드를 통해 `@types/react`, `@types/react-dom` 정확한 버전 지정
    
    - `tsconfig.json` 정렬 및 `"target": "ES2017"` 추가
    - `next.config.mjs`에서 `next-remove-imports`, `next-transpile-modules` 제거 (사용하지 않음)
    
    ※ 추후 App Router로 마이그레이션 예정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,3 @@
-import removeImports from 'next-remove-imports';
-import withTM from 'next-transpile-modules';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,11 +12,3 @@ const nextConfig = {
     ],
   },
 };
-
-// 불필요한 import 제거
-const withRemoveImports = removeImports();
-
-// 모듈 트랜스파일링기능 추가
-export default withRemoveImports(
-  withTM(['react-dom', 'react-icons'])(nextConfig)
-);
