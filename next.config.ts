b07b18851c958da2a36e71be09de239a8e4633eb
jngmnj <exorbitance@naver.com>
2025-10-30 21:21:13 +0900

    fix: Next.js Image에 Firebase Storage hostname 추가 (#56)
    - firebasestorage.googleapis.com을 remotePatterns에 추가
    - 이미지 최적화 및 로딩 에러 해결
    
    #56

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
