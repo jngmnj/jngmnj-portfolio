8d1913dfc23e6d23e6b0a2495486e5bfdace9c43
jngmnj <exorbitance@naver.com>
2026-02-26 00:59:46 +0900

    refactor: top banner i18n 적용(#70)
    - TopBanner에 i18n 텍스트 적용
    
    - topBanner locale 문자열 추가

--- src/components/common/TopBanner.tsx
+++ src/components/common/TopBanner.tsx
@@ -2,15 +2,17 @@
 
 import { useLocale } from '@/utils/useLocale';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 
 const TopBanner = () => {
   const lang = useLocale();
+  const { t } = useTranslation('common');
   return (
     <Link
       href={`/${lang}/contact`}
-      className="flex h-[40px] items-center justify-center bg-black text-white"
+      className="flex h-10 items-center justify-center bg-black text-white"
     >
-      I am open to new opportunities
+      {t('topBanner.text')}
     </Link>
   );
 };
